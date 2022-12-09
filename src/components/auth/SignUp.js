e22e2ed801a900cdd4869989f4287e6c66293036
Vlad Galayko <lorddarkvlad@gmail.com>
2022-12-09 22:12:56 +0200

    signIn

--- src/components/auth/SignUp.js
+++ src/components/auth/SignUp.js
@@ -1,47 +1,70 @@
-import React from 'react';
-import useInput from '../../hooks/useInput';
-import { Navigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import React, { Component } from 'react'
+import { Navigate } from 'react-router-dom'
+import { connect } from 'react-redux'
+import { signUp } from '../store/actions/authActions'
 
-const SignUp = () => {
-
-    const username = useInput('');
-    const password = useInput('');
-    const firstName = useInput('');
-    const lastName = useInput('');
-    const auth = useSelector(state => state.firebase.auth)
-
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        console.log(username.value, password.value, firstName.value, password.value)
-    }
-    if(auth.uid) return <Navigate to={'/'}/>
+class SignUp extends Component {
+  state = {
+    email: '',
+    password: '',
+    firstName: '',
+    lastName: '',
+  }
+  handleChange = (e) => {
+    this.setState({
+      [e.target.id]: e.target.value
+    })
+  }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    this.props.signUp(this.state);
+  }
+  render() {
+    const { auth, authError } = this.props;
+    if (auth.uid) return <Navigate to='/' /> 
     return (
-        <div className='container'>
-            <form onSubmit={handleSubmit} className="white">
-                <h5 className="grey-text text-darken-3">Sign Up</h5>
-                <div className="input-field">
-                    <label htmlFor="email">Email</label>
-                    <input {...username} type="email" id='email' />
-                </div>
-                <div className="input-field">
-                    <label htmlFor="password">Password</label>
-                    <input {...password} type="password" id='password' />
-                </div>
-                <div className="input-field">
-                    <label htmlFor="firstName">First Name</label>
-                    <input {...firstName} type="text" id='firstName' />
-                </div>
-                <div className="input-field">
-                    <label htmlFor="lastName">Last Name</label>
-                    <input {...lastName} type="text" id='lastName' />
-                </div>
-                <div className="input-field">
-                    <button className='btn pink lighten-1 z-depth-0'>Sign Up</button>
-                </div>
-            </form>
-        </div>
-    );
-};
+      <div className="container">
+        <form className="white" onSubmit={this.handleSubmit}>
+          <h5 className="grey-text text-darken-3">Sign Up</h5>
+          <div className="input-field">
+            <label htmlFor="email">Email</label>
+            <input type="email" id='email' onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <label htmlFor="password">Password</label>
+            <input type="password" id='password' onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <label htmlFor="firstName">First Name</label>
+            <input type="text" id='firstName' onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <label htmlFor="lastName">Last Name</label>
+            <input type="text" id='lastName' onChange={this.handleChange} />
+          </div>
+          <div className="input-field">
+            <button className="btn pink lighten-1 z-depth-0">Sign Up</button>
+            <div className="center red-text">
+              { authError ? <p>{authError}</p> : null }
+            </div>
+          </div>
+        </form>
+      </div>
+    )
+  }
+}
 
-export default SignUp;
+const mapStateToProps = (state) => {
+  return {
+    auth: state.firebase.auth,
+    authError: state.auth.authError
+  }
+}
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    signUp: (creds) => dispatch(signUp(creds))
+  }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps)(SignUp)
